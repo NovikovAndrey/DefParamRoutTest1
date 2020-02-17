08d1997e9c3df43c135395c16233abd947bafb2a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-17 11:50:10 +0300

    End

--- DefParamRoutTest1/Startup.cs
+++ DefParamRoutTest1/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,25 +17,26 @@ namespace DefParamRoutTest1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapRoute("Test/{action}/{id}", async context =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Test/{action}/{id} template used.");
             });
+            routeBuilder.MapRoute("{controller=Home}/{action}/{id}", async context =>
+            {
+                await context.Response.WriteAsync("{controller}/{action}/{id} template used.");
+            });
+            app.UseRouter(routeBuilder.Build());
+            app.Run(async (context) =>
+           {
+               await context.Response.WriteAsync("Def");
+           });
         }
     }
 }
